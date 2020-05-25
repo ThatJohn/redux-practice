d99aa07f6b5def2f6784bedde8aa4f1e4cd93bd5
ThatJohn <fajardocj@gmail.com>
2020-05-25 18:31:30 -0400

    Fixes constants usage

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,15 +1,15 @@
-import { ADD_ARTICLE } from '../constants/action-types';
+import { ADD_ARTICLE, FOUND_BAD_WORD } from '../constants/action-types';
 
 const forbiddenWords = ["money", "free"];
 
 export function forbiddenWordsMiddleware({dispatch}){
     return function(next){
         return function(action){
-            if(action.type == "ADD_ARTICLE"){
+            if(action.type == ADD_ARTICLE){
                 const foundWord = forbiddenWords.filter(word => action.payload.title.includes(word));
 
                 if(foundWord.length){
-                    return dispatch({type: "FOUND_BAD_WORD"});
+                    return dispatch({type: FOUND_BAD_WORD});
                 }
             }
             return next(action);
